8e01fa7d088b7672ab27147bfb69c59034624961
yejineee <21700431@handong.edu>
2019-05-29 04:32:03 +0900

    pthread_unlock안에서 mutex로 lock & complte ddtector.c
    Signed-off-by: yejineee <21700431@handong.edu>

--- PA3/ddtector.c
+++ PA3/ddtector.c
@@ -6,255 +6,375 @@
 #include <pthread.h>
 #include <unistd.h>
 #define TRUE 1
-#define FALSE -1
-/*
-# thread list
-1. thread별로 hold하고자 하는 mutex를 담기.
-2. 이미 thread에 있다면, 저장하지 않기
-# mutex list
-1. 
-*/
+#define FALSE 0
+void printMutexMatrix(void);
+void printThreadHold(pthread_t t);
+void push_ThreadInfo(pthread_t t, pthread_mutex_t *m);
+void push_ThreadHold(int idx, pthread_mutex_t *m, pthread_t t);
+void push_node(pthread_mutex_t *m);
+void push_edge(pthread_mutex_t* src, pthread_mutex_t* dst);
+void pop_ThreadHold(pthread_t t, pthread_mutex_t *m);
+void pop_MutexMatrix(pthread_mutex_t *m);
+
 typedef struct{
-	pthread_mutex_t* addr;
-	struct NODE *link;
+	pthread_mutex_t *mid;
 }NODE;
 
 typedef struct{
 	pthread_t tid;
-//	struct THREADLIST *link;
-	NODE* head;
-	NODE* tail;
 }THREADLIST;
-// 배열로 바꾸면 MUTEXLIST Struct 은 필요없어짐.
-typedef struct{
-	NODE* head;
-	struct MUTEXLIST *link;
-}MUTEXLIST;
 
-//static MUTEXLIST mutex_list;
-static THREADLIST* thread_list[10];
-//static NODE mutex_list[100];
+
+static THREADLIST thread_info[10];
+static NODE mutex_matrix[100][100];
+static NODE thread_hold[10][100];
+
+static NODE visited[100];
+static NODE stack[100];
+
+static int vIdx = 0;
+static int sIdx = 0;
+
 static int n_lock = 0;
 static int n_unlock = 0;
-static pthread_mutex_t MUTEX = PTHREAD_MUTEX_INITIALIZER;
- 
-NODE* newNode(pthread_mutex_t *item){
-	NODE* node = (NODE*)malloc(sizeof(NODE));
-	node->addr = item;
-	node->link = NULL;
-//	printf("newNode-addr : %u\n",node->addr);
-	return node;
+
+pthread_mutex_t mut = PTHREAD_MUTEX_INITIALIZER;
+
+int find_mutex_index(pthread_mutex_t* m){
+	int n;
+	for(n = 0; n < 100; n++){
+		if(mutex_matrix[n][0].mid == m){
+			return n;
+		}
+	}
+	//printf("cannot find mutex index\n");
 }
 
-THREADLIST* newThread(pthread_t t, NODE *node){
-	THREADLIST* thread = (THREADLIST*)malloc(sizeof(THREADLIST));
-	thread->tid = t;
-	thread->head = node;
-	thread->tail = node;
-/*	printf("----newThread----\n");
-	printf("nt-tid : %u\n",thread->tid);
-	printf("nt-head-node: %u\n",thread->head->addr);
-	printf("nt-tail-node: %u\n",thread->tail->addr);
-	printf("------------------\n");
-*/
-	return thread;
-}
-
-MUTEXLIST* newMutex(NODE *item){
-	MUTEXLIST* mutex = (MUTEXLIST*)malloc(sizeof(MUTEXLIST));
-	mutex->head = item;
-	mutex->link = NULL;
-/*	printf("----newMutex----\n");
-	printf("nM-head-node: %u\n",mutex->head->addr);
-	printf("------------------\n");
-*/
-	return mutex;
-}
-
-void insertInLockList (NODE* item){
-
-}
-
-void connectWithHold (NODE* hList, NODE* dst){
-
-}
-
-void printThreadList(pthread_t t){
+int is_visited(pthread_mutex_t* m){
 	int i;
-	for(i = 0; i < 10; i++){
-		if(thread_list[i] != NULL && thread_list[i]->tid == t)		
-		{
-		printf("%d>,<%u>",i,thread_list[i]->tid);
-		printList(thread_list[i]->head);
-		return;
+	for(i = 0; i < vIdx; i++){
+		if(visited[i].mid == m){
+			//printf("true-isvisited\n");
+			return TRUE;
 		}
 	}
+	//printf("mutex : %u | false-isvisited\n",m);
+	return FALSE;
 }
-
-			
-void printList(NODE* head){
-	int i ;
-	if(head == NULL){
-		return;
-	}
-	NODE* cur = head;
-	while(1){
-		printf("'%u'",cur->addr);
-		if(cur->link != NULL){
-			printf(" -> ");
-			cur = cur->link;
+int is_inStack(pthread_mutex_t* m){
+	int i;
+	for(i = 0; i < sIdx; i++){
+		if(stack[i].mid == m){
+			//printf("true-isInstack\n");
+			return TRUE;
 		}
-		else{
-			printf(" -> NIL\n");
+	}
+	//printf("mutex : %u | false-isInstack\n",m);
+	return FALSE;
+}
+void pop_stack(pthread_mutex_t* m){
+	int i;
+	for(i = 0; i < sIdx; i++){
+		if(stack[i].mid == m){
+			stack[i].mid = 0;
+			break;
+		}
+	}
+	sIdx-- ;
+}
+int check_cycle(int y, pthread_mutex_t* m){
+	int x;
+
+	if(!is_visited(m)){
+		visited[vIdx++].mid = m;
+		stack[sIdx++].mid = m;
+
+		for(x = 1; x < 100; x++){
+			if(mutex_matrix[y][x].mid != 0){
+				if(!is_visited(mutex_matrix[y][x].mid) &&
+				check_cycle(find_mutex_index(mutex_matrix[y][x].mid), mutex_matrix[y][x].mid)){
+				//	printf("DeadLock Detection 1 \n");
+					return TRUE;
+				}
+				else if(is_inStack(mutex_matrix[y][x].mid)){
+				//	printf("DeadLock Detection 2 \n");
+					return TRUE;
+				}
+			}
+		}
+	}
+	pop_stack(m);
+	return FALSE;
+
+}
+void is_cycle(void){
+	int y;
+	int i;
+	for(i = 0; i < vIdx; i++){
+		visited[i].mid = 0;
+	}
+	for(i = 0; i < sIdx; i++){
+		stack[i].mid = 0;
+	}
+	vIdx = 0;
+	sIdx = 0;
+
+	for(y = 0; y < 100; y++){
+		if( mutex_matrix[y][0].mid == 0) continue;
+		if(check_cycle(y,mutex_matrix[y][0].mid)){
+			printf("Deadlock Detection 3\n");
 			return;
 		}
 	}
+	return;
 }
-void delete_mutex_ThreadList(pthread_mutex_t *m){
+
+void print_visited(){
 	int i;
-	NODE *prev, *cur, *tmp ;
-	for(i = 0; i < 10; i++){
-		puts("1");
-		if(thread_list[i] != NULL){
-			puts("2");
-			if(thread_list[i]->head->addr == m){
-			//	전체 list 삭제
-				cur = thread_list[i]->head;
-				while(1){
-					tmp = cur;
-			puts("4");
-					if( cur->link != NULL){
-						puts("5");
-						free(tmp);
-						cur = cur->link;
-					}
-					else{
-						puts("6");
-						free(tmp);
-						break;
-					}
+	printf("vIdx : %d, visited : ",vIdx);
+	for(i = 0; i < 100; i++){
+		printf("(%d)%u > ", i,visited[i].mid);
+	}
+	printf("\n");
+}
+
+
+void push_edge(pthread_mutex_t* src, pthread_mutex_t* dst){
+	/*
+			mutex matrix[i][0] == src 인 곳에
+			 	dst가 있는지 확인.
+					있다면 -> return;
+					없다면 -> 빈 곳에 추가하고 return;
+	*/
+	int x, y;
+	for(y = 0; y < 100; y++){
+		if( mutex_matrix[y][0].mid == src){
+			for(x = 1; x < 100; x++){
+				if(mutex_matrix[y][x].mid == dst){
+					return;
 				}
-				free(thread_list[i]);
 			}
-			else{
-			/*
-			list traverse하면서 addr가 같은 Node는 연결 끊기
-			*/
-		//	for(i = 0; i < 10; i++){
-		//		if(thread_list[i] != NULL){
-					puts("7");
-					prev = NULL;
-					cur = thread_list[i]->head;
-					while(cur->addr != m){
-						puts("8");
-						prev = cur;
-						cur = cur->link;
-					}
-					if(cur == thread_list[i]->tail){
-						puts("9"); 
-						thread_list[i]->tail = prev;
-						prev->link = NULL;
-						free(cur);
-		//				continue;
-					}else{ 
-					puts("10");
-					prev->link = cur->link;
-					free(cur);
-					puts("11");
-					}
-		//	}
+			for(x = 1; x < 100; x++){
+				if(mutex_matrix[y][x].mid == 0){
+					mutex_matrix[y][x].mid = dst;
+					return;
+				}
 			}
 		}
 	}
 }
-void push_ThreadList(pthread_t t, pthread_mutex_t *m){
-	/* threadlist에 thread 정보가 있는지 확인
-	if 있다면, tail에 저장.
-	else 없다면, newThread를 저장.
+void push_node(pthread_mutex_t *m){
+	/*
+	mutex matrix[i][0] search하면서 m과 같은 것이 있는지 확인,
+	있다면 return;
+	없다면 matrix[i][0] = 0인 곳에 추가.
 	*/
 	int i;
-	NODE *new = newNode(m);
-	// check thread list if thread is already exist.
-	for(i=0; i<10; i++){
-	if(thread_list[i] != NULL && thread_list[i]->tid == t){
-			thread_list[i]->tail->link = new;
-			thread_list[i]->tail = new; 
+	// 이미 있는 경우 return;
+	for(i = 0; i < 100; i++){
+		if (mutex_matrix[i][0].mid == m){
 			return;
 		}
 	}
-	for(i=0; i<10; i++){
-		if(thread_list[i] == NULL){
-			thread_list[i] = newThread(t, new);
+	// mutex_matrix에 새롭게 추가
+	for(i = 0; i < 100; i++){
+		if(mutex_matrix[i][0].mid == 0){
+			mutex_matrix[i][0].mid = m;
 			return;
+		}
+	}
+}
+
+
+void push_ThreadHold(int idx, pthread_mutex_t *m, pthread_t t){
+	/* 이미 thread_hold에 m이 있다면 --> deadlock!
+		thread_hold에 없다면, mid 가 빈곳에 넣어줌
+	*/
+	int i;
+	for(i = 0; i < 100; i++){
+		if(thread_hold[idx][i].mid == m){ //self deadlock
+
+			printf("tid : %u | mid : %u | m : %u\n",t,thread_hold[idx][i].mid ,m);
+			printThreadHold(t);
+			printMutexMatrix();
+			printf("DEADLOCK OCCURS\n");
+
+			return;
+		}
+	}
+//	자신의 thread_hold의 모든 node에 대해서 edge 생성
+	for(i = 0; i < 100; i++){
+		if (thread_hold[idx][i].mid != 0){
+			push_edge(thread_hold[idx][i].mid, m);
 		}
 	}
 
-/*	THREADLIST* current = thread_list;
-	if(current == NULL){ 
-		thread_list = newThread(t, newNode(m));
-		return;
-	}		
-	while(current != NULL){
-		printf("current_tid = %u | t = %u\n",current->tid, t);
-		if(current->tid == t){
-			current->tail->link = newNode(m);
-			current->tail = current->tail->link;
+	for(i = 0; i < 100; i++){
+		if(thread_hold[idx][i].mid == 0){
+			thread_hold[idx][i].mid = m;
+			break;
+		}
+	}
+	// mutex_matrix에 이 mutex가 없는지 확인하고, 추가.
+	push_node(m);
+}
+void push_ThreadInfo(pthread_t t, pthread_mutex_t *m){
+	/* thread_info에 tid가 매칭되는 것을 찾아라.
+	if 있다면, 해당 thread_hold로 가서, thread_hold에 m 저장.
+	else 없다면, thread_info에 tid 저장하고, thread_hold에 m 저장.
+	*/
+	int i;
+	pthread_t target = t;
+	for(i = 0; i < 10; i++){
+		if(thread_info[i].tid == target){
+			//thread_hold로 가서 thread_hold에 m 저장
+			push_ThreadHold(i, m, t);
 			return;
 		}
-		current = current->link;
 	}
-	// thread list 에 없었으니, thread node를 추가한다.
-	current = newThread(t, newNode(m));*/
+	for(i = 0; i < 10; i++){
+		if(thread_info[i].tid == 0){ //빈 thread
+			//thread_hold로 가서 thread_hold에  m 저장
+			thread_info[i].tid = target;
+			push_ThreadHold(i, m, t);
+			return;
+		}
+	}
 }
-	
+void printThreadHold(pthread_t t){
+	int idx, j;
+	for( idx = 0; idx < 10; idx++){
+		if(thread_info[idx].tid == t){
+			printf("<tid : %u >",thread_info[idx].tid);
+			for(j = 0; j < 100; j++){
+				if(thread_hold[idx][j].mid != 0){
+					printf(" %u ->", thread_hold[idx][j].mid);
+				}
+			}
+			printf(" NIL\n");
+			return;
+		}
+	}
+}
+
+void printMutexMatrix(void){
+	int x, y;
+	//for( y = 0; y < 100; y++){
+	for( y = 0; y < 5; y++){
+		printf("%d:[%u] ",y,mutex_matrix[y][0].mid);
+		//for( x = 1; x < 100; x++){
+		for( x = 1; x < 5; x++){
+			printf("'%u' ",mutex_matrix[y][x].mid);
+		}
+		printf("\n");
+	}
+}
+void pop_MutexMatrix(pthread_mutex_t *m){
+	/*
+
+	for y = 0 to 100
+		if mm[y][0] == m;
+			for x = 0 to x 100 delete all
+		else
+			for x = 1 to 100
+			 if mm[y][x] == m 이면 delete.
+	*/
+	int x, y;
+	for(y = 0; y < 100; y++){
+		if(mutex_matrix[y][0].mid == m){
+			for(x = 0; x < 100; x++){
+					mutex_matrix[y][x].mid = 0;
+			}
+		}
+		else{
+			for(x = 1; x < 100; x++){
+				if(mutex_matrix[y][x].mid == m){
+					mutex_matrix[y][x].mid = 0;
+					break;
+				}
+			}
+		}
+	}
+}
+
+void pop_ThreadHold(pthread_t t, pthread_mutex_t *m){
+	/*
+		thread_hold에서 해당 mutex 제거
+	*/
+	int idx, x;
+	for(idx = 0; idx < 10; idx++){
+		if(thread_info[idx].tid == t){
+			for(x = 0; x < 100; x++){
+				if(thread_hold[idx][x].mid == m){
+					thread_hold[idx][x].mid = 0;
+					return;
+				}
+			}
+		//	printf("error : not found mid in thread_hold\n");
+		}
+	}
+//	printf("error : not found tid in thread_info\n");
+}
+
 int
 pthread_mutex_lock (pthread_mutex_t *mutex)
 {
+	int (*orig_lock)(pthread_mutex_t *mutex);
+	int (*orig_unlock)(pthread_mutex_t *mutex);
+	char *error;
+	int i;
+	orig_lock = dlsym(RTLD_NEXT, "pthread_mutex_lock");
+	if((error = dlerror()) != 0x0){
+		exit(1);
+	}
+	orig_unlock = dlsym(RTLD_NEXT, "pthread_mutex_unlock");
+	if((error = dlerror()) != 0x0){
+		exit(1);
+	}
+	orig_lock(&mut);
+		pthread_t tid = pthread_self();
+	//	printf("--------------tid: <%u>, mutex : '%u'--------\n",tid, mutex);
+		push_ThreadInfo(tid, mutex);
+		is_cycle();
+		//print_visited();
+	//	printThreadHold(tid);
+//		printMutexMatrix();
+		//printf("lock!!!! tid:%u | mutex : %u\n",tid,mutex);
+//		printThreadHold(tid);
+		//printMutexMatrix();
+//		printf("----------------------------------------\n");
+	orig_unlock(&mut);
 
-//static THREADLIST* thread_list[10];
-//static NODE mutex_list[100];
-//static int n_lock = 0;
+	return orig_lock(mutex) ;
+}
 
-	int (*orig_lock)(pthread_mutex_t *mutex); 
+int
+pthread_mutex_unlock (pthread_mutex_t *mutex)
+{
+	int (*orig_lock)(pthread_mutex_t *mutex);
+	int (*orig_unlock)(pthread_mutex_t *mutex);
 	char *error;
 	orig_lock = dlsym(RTLD_NEXT, "pthread_mutex_lock");
 	if((error = dlerror()) != 0x0){
 		exit(1);
 	}
-//	orig_lock(&MUTEX);
-	pthread_t tid = pthread_self();
-//	printf("-------------------tid : %u  ",tid);
-//	printf("+mutex: %u\n------------------", mutex);	
-	n_lock++;
-	if( n_lock == 1){
-	
-	push_ThreadList(tid, mutex);	
-	printf("tid: %u =========lock <%u>=========\n", tid,mutex);
-	printThreadList(tid);
-	}
-	n_lock--;
-//	printThreadList(tid);
-	
-	return orig_lock(mutex) ; 	
-}
-
-int 
-pthread_mutex_unlock (pthread_mutex_t *mutex)
-{
-	int (*orig_unlock)(pthread_mutex_t *mutex);
-	char * error;
-	orig_unlock = dlsym(RTLD_NEXT,"pthread_mutex_unlock");
-	if((error = dlerror()) != 0x0)
+	orig_unlock = dlsym(RTLD_NEXT, "pthread_mutex_unlock");
+	if((error = dlerror()) != 0x0){
 		exit(1);
-	n_unlock++;
-	pthread_t tid = pthread_self();
-	if(n_unlock == 1){
-	printf("tid: %u ---unlock: %u---\n",tid, mutex);
-	delete_mutex_ThreadList(mutex);
-//	sleep(10);
-	printThreadList(pthread_self());
 	}
-	n_unlock--;
+	orig_lock(&mut);
+	pthread_t tid = pthread_self();	
+//	printf("unlock---------tid: <%u>, mutex : '%u'--------\n",tid, mutex);
+	  pop_MutexMatrix(mutex);
+	  pop_ThreadHold(tid, mutex);
 	
+	//	printf("unlock tid:%u | mutex : %u\n",tid,mutex);
+	//	printMutexMatrix();
+	//	printThreadHold(tid);
+	//	printf("--------------------------\n");
+//	printf("unlock\n",tid, mutex);
+//  printThreadHold(tid);
+	//printMutexMatrix();
+	orig_unlock(&mut);
 	return orig_unlock(mutex);
 }
